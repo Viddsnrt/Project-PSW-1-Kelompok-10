bb010530e68b8440769dc17a37cac59b932112c8
Bintang Jesisca Tanjung <bintang4435@sma.belajar.id>
2024-11-29 10:45:51 +0700

    update Sifat Koligatif js dan css, Tambah RumusOsmotik.js

---
+++ src/materi/RumusOsmotik.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './DeltaTb.css';
+
+function RumusOsmotik() {
+  return (
+    <div className="persamaan-container">
+      <p className="persamaan">
+      π = icRT
+      </p>
+    </div>
+  );
+}
+
+export default RumusOsmotik;
