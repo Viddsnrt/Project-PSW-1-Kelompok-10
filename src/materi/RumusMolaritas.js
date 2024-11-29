417abfe6358fcdc56e27ece0dd45b4948ec947dc
Bintang Jesisca Tanjung <bintang4435@sma.belajar.id>
2024-11-29 21:25:38 +0700

    update rumus dan tambah tabel
    update sifat koligatif, tambah rumus molalitas, tambah rumus molaritas, tambah tabel molaritas dan molalitas

---
+++ src/materi/RumusMolaritas.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './DeltaTb.css';
+
+function RumusMolaritas() {
+  return (
+    <div className="persamaan-container">
+      <p className="persamaan">
+      molaritas (M) = jumlah mol zat terlarut / volume larutan (L)
+      </p>
+    </div>
+  );
+}
+
+export default RumusMolaritas;
